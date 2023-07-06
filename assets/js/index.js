f9cb9bcbc7668b34213ebcb3b97b6a6752b85549
Syraff <72563332+Syraff@users.noreply.github.com>
2023-07-06 12:29:38 +0800

    70%

--- assets/js/index.js
+++ assets/js/index.js
@@ -106,7 +106,7 @@ const anyQuestions = [
       "Ekstrak rosmarin",
       "Minyak kacang-kacangan",
       "Ekstrak kacang kedelai",
-      "Aku punya alergi sebelumnya, tetapi kurang yakin apa penyebabnya",
+      "Aku punya alergi sebelumnya",
       "Tidak ada alergi sejauh ini",
     ],
     category: "Your Skin",
@@ -132,12 +132,88 @@ function render() {
   });
   let submitBtn = document.getElementById("btnSubmit");
   let question = document.getElementById("question");
+  let category = document.getElementById("category");
   let number = 0;
+  category.textContent = anyQuestions[number].category;
   question.textContent = anyQuestions[number].question;
+
+  //styling
+  if (anyQuestions[number].category === "Your Skin") {
+    document.body.style.background = "#ffede0";
+  }
+
+  //looping
+  function looping() {
+    if (number === 4 || number === 7) {
+      document.getElementById("form").className = "form";
+      let allCard = document.createElement("div");
+      for (i = 0; i < anyQuestions[number].answer.length; i++) {
+        let component = document.createElement("div");
+        let answer = anyQuestions[number].answer[i];
+        let img = document.createElement("img");
+        let jawaban = document.createElement("p");
+        allCard.className = "all-card";
+        allCard.id = "all-card";
+        img.src = answer[1];
+        component.id = "input";
+        component.onclick = () => handleClick(answer[0]);
+        component.className = "card";
+        jawaban.innerHTML = answer[0];
+        allCard.appendChild(component);
+        component.appendChild(img);
+        component.appendChild(jawaban);
+        document.getElementById("form").appendChild(allCard);
+      }
+    } else {
+      let component = document.createElement("div");
+      for (i = 0; i < anyQuestions[number].answer.length; i++) {
+        let answer = anyQuestions[number].answer[i];
+        let jawaban = document.createElement("input");
+        component.id = "input";
+        component.className = "flex";
+        jawaban.type = "button";
+        jawaban.value = answer;
+        jawaban.id = answer;
+        jawaban.className = "answer";
+        jawaban.onclick = () => handleClick(answer);
+        component.appendChild(jawaban);
+        document.getElementById("form").appendChild(component);
+      }
+    }
+  }
+
+  //handle click
 
   function handleClick(value) {
     console.log(value);
+    if (anyQuestions[number].category === "About Yourself") {
+      if (value === "Wanita") {
+        number++;
+      } else {
+        number += 2;
+      }
+      obj["gender"] = value;
+      let input = document.getElementById("input");
+      input.remove();
+      looping();
+    } else if (anyQuestions[number].category === "Your Skin") {
+      obj["Your Skin"].push(value);
+      if (number === 4 || number === 7) {
+        let input = document.getElementById("all-card");
+        input.remove();
+      } else {
+        let input = document.getElementById("input");
+        input.remove();
+      }
+      number++;
+      looping();
+    }
+    category.textContent = anyQuestions[number].category;
+    question.textContent = anyQuestions[number].question;
+    console.log(obj);
   }
+
+  //fisrt submit
 
   submitBtn.addEventListener("click", () => {
     if (question.textContent === anyQuestions[0].question) {
@@ -156,22 +232,12 @@ function render() {
       iconSubmit.style.display = "none";
     }
     if (number > 1) {
-      for (i = 0; i < anyQuestions[number].answer.length; i++) {
-        let answer = anyQuestions[number].answer[i];
-        document.getElementById("form").className = "flex";
-        let jawaban = document.createElement("input");
-        jawaban.type = "button";
-        jawaban.value = answer;
-        jawaban.id = answer;
-        jawaban.className = "answer";
-        jawaban.onclick = handleClick(answer);
-        document.getElementById("form").appendChild(jawaban);
-      }
+      looping();
     }
+    category.textContent = anyQuestions[number].category;
     question.textContent = anyQuestions[number].question;
     console.log(obj);
     console.log(question.textContent);
   });
-  console.log(number);
 }
 render();
